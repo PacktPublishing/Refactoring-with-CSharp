c123efd3bf8a0c10159330047974cd8ee9410905
Matt Eland <matt.eland@gmail.com>
2023-06-28 22:13:12 -0400

    Condensed code to fit width of page

--- Chapter02/Ch2BeginningCode/Chapter2/BaggageCalculator.cs
+++ Chapter02/Ch2BeginningCode/Chapter2/BaggageCalculator.cs
@@ -1,46 +1,50 @@
 ﻿namespace Packt.CloudySkiesAir.Chapter2;
 
 public class BaggageCalculator {
-    private decimal holidayTravelFeePercent = 0.1M;
-    public decimal HolidayTravelFeePercent {
-        get { return holidayTravelFeePercent; }
-        set { holidayTravelFeePercent = value; }
+
+  private decimal holidayTravelFeePercent = 0.1M;
+  public decimal HolidayTravelFeePercent {
+    get { return holidayTravelFeePercent; }
+    set { holidayTravelFeePercent = value; }
+  }
+
+  public decimal CalculatePrice(int bags, 
+    int carryOn, int passengers, DateTime travelTime) {
+
+    decimal total = 0;
+
+    if (carryOn > 0) {
+      Console.WriteLine($"Carry-on: {carryOn * 30M}");
+      total += carryOn * 30M;
     }
 
-    public decimal CalculatePrice(int numChecked, int numCarryOn,
-        int numPassengers, DateTime travelTime) {
-        decimal total = 0;
+    if (bags > 0) {
+      if (bags <= passengers) {
+        Console.WriteLine($"Checked: {bags * 40M}");
+        total += bags * 40M;
+      } else {
+        decimal checkedFee = (passengers * 40M) + 
+          ((bags - passengers) * 50M);
 
-        if (numCarryOn > 0) {
-            Console.WriteLine($"Carry-on bag price: {numCarryOn * 30M}");
-            total += numCarryOn * 30M;
-        }
-
-        if (numChecked > 0) {
-            if (numChecked <= numPassengers) {
-                Console.WriteLine($"Checked bag price: {numChecked * 40M}");
-                total += numChecked * 40M;
-            } else {
-                decimal checkedFee = (numPassengers * 40M) + ((numChecked - numPassengers) * 50M);
-                Console.WriteLine($"Checked bag price: {checkedFee}");
-                total += checkedFee;
-            }
-        }
-
-        if (travelTime.Month >= 11 || travelTime.Month <= 2) {
-            Console.WriteLine($"Holiday Fee: {total * HolidayTravelFeePercent}");
-            total += total * HolidayTravelFeePercent;
-        }
-
-        return total;
+        Console.WriteLine($"Checked: {checkedFee}");
+        total += checkedFee;
+      }
     }
-    private decimal CalculatePriceFlat(int numBags) {
-        decimal total = 0;
 
-        // Business says to use a flat 100 regardless of count
-        return 100M;
+    if (travelTime.Month >= 11 || travelTime.Month <= 2) {
+      Console.WriteLine("Holiday Fee: " + 
+        (total * HolidayTravelFeePercent));
 
-        // Old logic: $50 per bag
-        return numBags * 50M;
+      total += total * HolidayTravelFeePercent;
     }
+
+    return total;
+  }
+  private decimal CalculatePriceFlat(int numBags) {
+    decimal total = 0;
+
+    return 100M;
+
+    return numBags * 50M;
+  }
 }
