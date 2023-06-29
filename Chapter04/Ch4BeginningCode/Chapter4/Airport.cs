02d41552c3b81d8f2d6c1d061eb02e7899c62a91
Matt Eland <matt.eland@gmail.com>
2023-06-28 23:56:45 -0400

    Starting on Chapter 4 code

---
+++ Chapter04/Ch4BeginningCode/Chapter4/Airport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.CloudySkiesAir.Chapter4; 
+
+public class Airport {
+
+  public string Code { get; set; }
+  public string Name { get; set; }
+
+  public List<Runway> Runways { get; set; }
+}
+
+public class Runway {
+  public string Name { get; set; }
+  public decimal LengthInMeters { get; set; }
+  public bool HasApproachLights { get; set; }
+  public string SurfaceType { get; set; }
+}
---
+++ Chapter04/Ch4BeginningCode/Chapter4/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Packt.CloudySkiesAir.Chapter4;
+
+internal class Program
+{
+    private static void Main()
+    {
+    }
+}
