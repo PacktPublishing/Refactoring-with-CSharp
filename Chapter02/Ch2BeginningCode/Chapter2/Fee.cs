d8d370abe9ea2b2b864967cb311e26696c42de11
Matt Eland <matt.eland@gmail.com>
2023-06-28 23:02:48 -0400

    Added sample fee file (fo fum)

---
+++ Chapter02/Ch2BeginningCode/Chapter2/Fee.cs
@@ -0,0 +1,14 @@
+﻿namespace Packt.CloudySkiesAir.Chapter2; 
+
+public class Fee {
+  public decimal Total { get; set; }
+
+  public void ChargeCarryOnBaggageFee(decimal fee) {
+    Console.WriteLine($"Carry-on Fee: {fee}");
+    Total += fee;
+  }
+  public void ChargeCheckedBaggageFee(decimal fee) {
+    Console.WriteLine($"Checked Fee: {fee}");
+    Total += fee;
+  }
+}
