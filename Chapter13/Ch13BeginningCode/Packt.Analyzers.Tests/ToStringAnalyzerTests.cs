0df1a064836dd46e9bb66db11ca571fd38322c4b
Matt Eland <matt.eland@gmail.com>
2023-09-04 16:35:36 -0400

    Ready to add VSIX

---
+++ Chapter13/Ch13BeginningCode/Packt.Analyzers.Tests/ToStringAnalyzerTests.cs
@@ -0,0 +1,46 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using RoslynTestKit;
+
+namespace Packt.Analyzers.Tests; 
+
+public class ToStringAnalyzerTests : AnalyzerTestFixture {
+    protected override string LanguageName
+        => LanguageNames.CSharp;
+
+    protected override DiagnosticAnalyzer CreateAnalyzer()
+        => new ToStringAnalyzer();
+
+    public const string GoodCode = @"
+using System;
+public class Flight
+{
+  public string Id {get; set;}
+  public string DepartAirport {get; set;}
+  public string ArriveAirport {get; set;}
+  public DateTime DepartTime {get; set;}
+  public DateTime ArriveTime {get; set;}
+  public override string ToString() => Id;
+}";
+
+    [Fact]
+    public void AnalyzerShouldNotFlagGoodCode() {
+        NoDiagnostic(GoodCode, ToStringAnalyzer.Rule.Id);
+    }
+
+    public const string BadCode = @"
+using System;
+public class [|Flight|]
+{
+  public string Id {get; set;}
+  public string DepartAirport {get; set;}
+  public string ArriveAirport {get; set;}
+  public DateTime DepartTime {get; set;}
+  public DateTime ArriveTime {get; set;}
+}";
+
+    [Fact]
+    public void AnalyzerShouldFlagViolations() {
+        HasDiagnostic(BadCode, ToStringAnalyzer.Rule.Id);
+    }
+}
