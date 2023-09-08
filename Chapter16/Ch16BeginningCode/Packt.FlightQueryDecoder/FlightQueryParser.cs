76f860b26519b35782df97a04092bc58cd77b074
Matt Eland <matt.eland@gmail.com>
2023-09-08 16:50:00 -0400

    Ch16 Styling changes for print

--- Chapter16/Ch16BeginningCode/Packt.FlightQueryDecoder/FlightQueryParser.cs
+++ Chapter16/Ch16BeginningCode/Packt.FlightQueryDecoder/FlightQueryParser.cs
@@ -1,44 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Packt.FlightQueryDecoder; 
 
-namespace Packt.FlightQueryDecoder {
-    public class FlightQueryParser {
+public class FlightQueryParser 
+{
 
-        // DEN LHR 430P 830A+1 E0/781 9:00
-        // DEN LHR 550P 940A+1 E0/789 8:50
-
-        public FlightQuery ParseQuery(string query) {
-            if (query.StartsWith("AD") && query.Length == 13) {
-                var flightQuery = new FlightQuery {
-                    Date = DateTime.ParseExact(query.Substring(2, 5), "ddMMM", CultureInfo.InvariantCulture),
-                    Origin = query.Substring(7, 3),
-                    Destination = query.Substring(10, 3)
-                };
-                return flightQuery;
-            } else {
-                throw new ArgumentException("Invalid flight query format.", "query");
-            }
-        }
-
-        public FlightQueryResult ParseResult(string result) {
-            var flightQueryResult = new FlightQueryResult();
-
-            var segments = result.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-            flightQueryResult.Origin = segments[0];
-            flightQueryResult.Destination = segments[1];
-
-            string today = DateTime.Today.ToShortDateString();
-            flightQueryResult.DepartureTime = DateTime.Parse(DateTime.Today.ToShortDateString() + " " + segments[2] + 'M');
-            flightQueryResult.ArrivalTime = DateTime.Parse(DateTime.Today.ToShortDateString() + " " + segments[3] + 'M');
-            flightQueryResult.AircraftTypeDesignator = segments[4];
-            flightQueryResult.FlightDuration = TimeSpan.Parse(segments[5]);
-
-            return flightQueryResult;
+    public FlightQuery ParseQuery(string query) {
+        if (query.StartsWith("AD") && query.Length == 13) 
+        {
+            var flightQuery = new FlightQuery {
+                Date = DateTime.Parse(query.Substring(2, 5)),
+                Origin = query.Substring(7, 3),
+                Destination = query.Substring(10, 3)
+            };
+            return flightQuery;
+        } 
+        else {
+            throw new ArgumentException("Invalid query format.");
         }
     }
+
+    public FlightQueryResult ParseResult(string result) {
+        var fqr = new FlightQueryResult();
+        var segments = result.Split(' ',
+          StringSplitOptions.RemoveEmptyEntries
+          | StringSplitOptions.TrimEntries);
+        fqr.Origin = segments[0];
+        fqr.Destination = segments[1];
+        string today = DateTime.Today.ToShortDateString();
+        fqr.DepartureTime = DateTime.Parse(
+          today + " " + segments[2] + 'M');
+        string seg3 = segments[3];
+        fqr.ArrivalTime = DateTime.Parse($"{today} {seg3}M");
+        fqr.AircraftTypeDesignator = segments[4];
+        fqr.FlightDuration = TimeSpan.Parse(segments[5]);
+        return fqr;
+    }
+
 }
