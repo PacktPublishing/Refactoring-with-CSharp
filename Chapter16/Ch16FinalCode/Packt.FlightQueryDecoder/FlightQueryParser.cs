74ba27365982457a2719c35f58f2471c3903420a
Matt Eland <matt.eland@gmail.com>
2023-09-13 22:37:41 -0400

    Ch16 Final

---
+++ Chapter16/Ch16FinalCode/Packt.FlightQueryDecoder/FlightQueryParser.cs
@@ -0,0 +1,43 @@
+﻿namespace Packt.FlightQueryDecoder; 
+
+public class FlightQueryParser 
+{
+
+    public FlightQuery ParseQuery(string query) {
+        if (query.StartsWith("AD") && query.Length == 13) 
+        {
+            var flightQuery = new FlightQuery {
+                Date = DateTime.Parse(query.Substring(2, 5)),
+                Origin = query.Substring(7, 3),
+                Destination = query.Substring(10, 3)
+            };
+            return flightQuery;
+        } 
+        else {
+            throw new ArgumentException("Invalid query format.");
+        }
+    }
+
+    public FlightQueryResult ParseResult(string result) {
+        var fqr = new FlightQueryResult();
+
+        var segments = result.Split(' ',
+          StringSplitOptions.RemoveEmptyEntries
+          | StringSplitOptions.TrimEntries);
+
+        fqr.Origin = segments[0];
+        fqr.Destination = segments[1];
+
+        string today = DateTime.Today.ToShortDateString();
+        fqr.DepartureTime = DateTime.Parse(
+          today + " " + segments[2] + 'M');
+
+        string seg3 = segments[3];
+        fqr.ArrivalTime = DateTime.Parse($"{today} {seg3}M");
+        fqr.AircraftTypeDesignator = segments[4];
+        fqr.FlightDuration = TimeSpan.Parse(segments[5]);
+
+        return fqr;
+    }
+
+}
---
+++ Chapter16/Ch16FinalCode/Packt.FlightQueryDecoder/FlightQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Packt.FlightQueryDecoder {
+    public class FlightQueryResult {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string AircraftTypeDesignator { get; internal set; }
+        public TimeSpan FlightDuration { get; internal set; }
+        public DateTime ArrivalTime { get; internal set; }
+        public DateTime DepartureTime { get; internal set; }
+    }
+}
